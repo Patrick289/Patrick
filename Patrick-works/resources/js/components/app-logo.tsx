fe7d472a2997db5c50a2856db9e285b80c5d630b
Junn Xavier Adalid <xaviadalid959@gmail.com>
2026-07-11 16:38:47 +0800

    v1 modified name

--- Patrick-works/resources/js/components/app-logo.tsx
+++ Patrick-works/resources/js/components/app-logo.tsx
@@ -8,7 +8,7 @@ export default function AppLogo() {
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Patrick Build A System
+                    Patrick System
                 </span>
             </div>
         </>
